45d1a1dd12e7d8b86080a3659f7a6cbc5c2d783d
Luiz Costa <luiz.costa@mirante.com.br>
2023-08-09 17:57:05 -0300

    feat: insert features on project - checklist controllers and Tarefa controlers

---
+++ src/controllers/TarefaController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+import { prisma } from "../database/prisma"
+
+export const createTarefa = async (req: Request, res: Response) => {
+  const {description, verificado, foto_verificado} = req.body
+
+  const tarefa = await prisma.tarefa.create({
+    data: {
+      description,
+      verificado,
+      foto_verificado
+    }
+  });
+
+  return res.json(tarefa)
+};
+
+export const getAllTarefas = async (req: Request, res: Response) => {
+  const tarefas = await prisma.tarefa.findMany()
+  return res.json(tarefas);
+}
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUser } from "./controllers/UserController";
 import { createAccess, getAllAccesses } from "./controllers/AcessController";
-import { createAntena } from "./controllers/AntenaController";
+import { createAntena, getAllAntenas } from "./controllers/AntenaController";
 import { createTipoEquipamento, getAllTipoEquipamentos } from "./controllers/TipoEquipamentoController";
+import { createTarefa, getAllTarefas } from "./controllers/TarefaController";
+import { createChecklist, getAllChecklists } from "./controllers/ChecklistController";
 
 
 export const router = Router()
@@ -10,7 +12,16 @@ export const router = Router()
 router.post("/user", createUser)
 
 router.post("/access", createAccess)
-router.post("/antena", createAntena)
-router.post("/tipo-equipamento", createTipoEquipamento)
 router.get("/accesses", getAllAccesses)
+
+router.get("/antenas", getAllAntenas)
+router.post("/antena", createAntena)
+
+router.post("/tarefa", createTarefa)
+router.get("/tarefas", getAllTarefas)
+
+router.post("/tipo-equipamento", createTipoEquipamento)
 router.get("/tipo-equipamentos", getAllTipoEquipamentos)
+
+router.post("/checklist", createChecklist)
+router.get("/checklists", getAllChecklists)
